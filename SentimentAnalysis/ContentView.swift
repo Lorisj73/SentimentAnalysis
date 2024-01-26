279285c7796f5beca7e3c0b21b0d4d755970dd49
Lorisj73 <lorisj73@gmail.com>
2024-01-26 10:26:09 +0100

    Initial Commit

---
+++ SentimentAnalysis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SentimentAnalysis
+//
+//  Created by Loris JACOB on 1/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SentimentAnalysis/SentimentAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalysisApp.swift
+//  SentimentAnalysis
+//
+//  Created by Loris JACOB on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SentimentAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
